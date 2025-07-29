8746ec251f03683b38a9f31d2a4891d0abff1d5c
Marcus Botelho <marcus.itec@gmail.com>
2025-07-29 17:27:08 -0300

    feat: configura lazy loading para módulo de cursos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./features/courses/courses.routes').then(m => m.COURSES_ROUTES)
+  },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' }
+];
