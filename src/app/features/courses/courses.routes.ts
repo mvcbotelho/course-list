3e41c6a34bacbefe76eec838efe3a78563440503
Marcus Botelho <marcus.itec@gmail.com>
2025-08-05 15:20:01 -0300

    feat: atualiza rotas com guards de autenticação

--- src/app/features/courses/courses.routes.ts
+++ src/app/features/courses/courses.routes.ts
@@ -1,6 +1,20 @@
 import { Routes } from '@angular/router';
 import { CourseListComponent } from './pages/course-list/course-list';
+import { CourseDetailsComponent } from './pages/course-details/course-details';
+import { CourseFormComponent } from './pages/course-form/course-form';
+import { AuthGuard } from '../../guards/auth.guard';
 
 export const COURSES_ROUTES: Routes = [
-  { path: '', component: CourseListComponent }
+  { path: '', component: CourseListComponent },
+  {
+    path: 'new',
+    component: CourseFormComponent,
+    canActivate: [AuthGuard] // Protege criação de novos cursos
+  },
+  { path: ':id', component: CourseDetailsComponent },
+  {
+    path: ':id/edit',
+    component: CourseFormComponent,
+    canActivate: [AuthGuard] // Protege edição de cursos
+  }
 ];
